7acac40f11d55bf7a90e0fbc6c5c67c08b5e9d4e
laveen <laveen.ekka@gmail.com>
2018-04-14 20:35:31 +0530

    Duplicate Question Pair Detection

---
+++ models/make_test_train_data_from_given_quora_dataset.py
@@ -0,0 +1,44 @@
+import sys
+
+fin = sys.argv[1]
+
+#fouttr = sys.argv[3]
+#fouttt = sys.argv[4]
+
+fwtr = open("train.csv", "w")
+fwtt = open("test.csv", "w")
+
+
+frtr = open(fin, "r", encoding="utf8")
+
+
+
+lines = frtr.readlines()
+
+tot_lines = 0
+
+
+
+for j, line in enumerate(lines):
+	if len(line) > 1:
+		#print(j)
+		tot_lines += 1
+
+
+tr_l = int(0.7 * tot_lines)
+tt_l = tot_lines - tr_l
+
+
+for j, line in enumerate(lines):
+	if len(line)>1:
+		print(j)
+		line  = line.strip()
+		if j==0:
+			fwtr.write(line + '\n')
+			fwtt.write(line + '\n')
+		elif j <= tr_l:
+			fwtr.write(line + '\n')
+		else:
+			fwtt.write(line + '\n')
+
+
---
+++ models/siamese_lstm_char.py
@@ -0,0 +1,321 @@
+from time import time
+import pandas as pd
+import numpy as np
+import re
+from nltk.corpus import stopwords
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras import metrics
+import sys
+import os
+import pickle
+
+import itertools
+import datetime
+
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+from keras.layers import Input, Embedding, LSTM, Merge
+from keras.models import load_model
+import keras.backend as K
+from keras.optimizers import Adadelta
+from keras.callbacks import ModelCheckpoint
+
+
+stops = set(stopwords.words('english'))
+char_model = {}
+
+
+def loadModel(gloveFile):
+	"""
+	:param : glove file contains vectors representation of almost all words.
+	:Add the representaion of words in dict i.e. model
+	"""
+	global char_model
+	f = open(gloveFile,'r')
+	for line in f:
+		splitLine = line.split()
+		char = splitLine[0]
+		embedding = np.array([float(val) for val in splitLine[1:]])
+		char_model[char] = embedding
+	with open('char_model.pickle', 'wb') as handle:
+		pickle.dump(char_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+	return char_model
+
+
+def text_to_char_list(text):
+	''' Pre process and convert texts to a list of chars '''
+	text = str(text)
+	text = text.lower()
+
+	# Clean the text
+	text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+	text = re.sub(r"what's", "what is ", text)
+	text = re.sub(r"\'s", " ", text)
+	text = re.sub(r"\'ve", " have ", text)
+	text = re.sub(r"can't", "cannot ", text)
+	text = re.sub(r"n't", " not ", text)
+	text = re.sub(r"i'm", "i am ", text)
+	text = re.sub(r"\'re", " are ", text)
+	text = re.sub(r"\'d", " would ", text)
+	text = re.sub(r"\'ll", " will ", text)
+	text = re.sub(r",", " ", text)
+	text = re.sub(r"\.", " ", text)
+	text = re.sub(r"!", " ! ", text)
+	text = re.sub(r"\/", " ", text)
+	text = re.sub(r"\^", " ^ ", text)
+	text = re.sub(r"\+", " + ", text)
+	text = re.sub(r"\-", " - ", text)
+	text = re.sub(r"\=", " = ", text)
+	text = re.sub(r"'", " ", text)
+	text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+	text = re.sub(r":", " : ", text)
+	text = re.sub(r" e g ", " eg ", text)
+	text = re.sub(r" b g ", " bg ", text)
+	text = re.sub(r" u s ", " american ", text)
+	text = re.sub(r"\0s", "0", text)
+	text = re.sub(r" 9 11 ", "911", text)
+	text = re.sub(r"e - mail", "email", text)
+	text = re.sub(r"j k", "jk", text)
+	text = re.sub(r"\s{2,}", " ", text)
+
+	char_list = []
+	for char in text:
+		char_list.append(char)
+
+	return char_list
+
+
+if (sys.argv[1] == 'train'):
+	
+	char_model
+	# File paths
+	TRAIN_CSV = './train.csv'
+	TEST_CSV = './test.csv'
+
+	# Load training and test set
+	train_df = pd.read_csv(TRAIN_CSV)
+	test_df = pd.read_csv(TEST_CSV)
+
+	# loading pre-traing char2vec
+	if os.path.isfile('char_model.pickle'):
+		handle = open('char_model.pickle', 'rb')
+		char_model = pickle.load(handle, encoding='latin1')
+	else:
+		char_model = loadModel('./glove.6B.300d-char.txt')
+	
+	# Prepare embedding
+	vocabulary = dict()
+	inverse_vocabulary = ['<unk>']  # '<unk>' will never be used, it is only a placeholder for the [0, 0, ....0] embedding
+
+	questions_cols = ['question1', 'question2']
+
+	# Iterate over the questions only of both training and test datasets
+	for dataset in [train_df, test_df]:
+		for index, row in dataset.iterrows():
+			# Iterate through the text of both questions of the row
+			for question in questions_cols:
+			    q2n = []  # q2n -> question numbers representation
+			    for char in text_to_char_list(row[question]):
+
+			        # Check for unwanted words
+			        if char not in char_model:
+			            continue
+
+			        if char not in vocabulary:
+			            vocabulary[char] = len(inverse_vocabulary)
+			            q2n.append(len(inverse_vocabulary))
+			            inverse_vocabulary.append(char)
+			        else:
+			            q2n.append(vocabulary[char])
+
+			    # Replace questions as char to question as number representation
+			    dataset.set_value(index, question, q2n)
+
+		
+	embedding_dim = 300
+	embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)  # This will be the embedding matrix
+	embeddings[0] = 0  # So that the padding will be ignored
+
+	# Build the embedding matrix
+	for char, index in vocabulary.items():
+		if char in char_model:
+			embeddings[index] = char_model[char]		
+
+	max_seq_length = 500
+
+	# Split to train validation
+	validation_size = int(0.15 * len(train_df))
+	training_size = len(train_df) - validation_size
+
+	X = train_df[questions_cols]
+	Y = train_df['is_duplicate']
+
+	X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size)
+
+	print ('training stared')
+	# Split to dicts
+	X_train = {'left': X_train.question1, 'right': X_train.question2}
+	X_validation = {'left': X_validation.question1, 'right': X_validation.question2}
+	X_test = {'left': test_df.question1, 'right': test_df.question2}
+
+	# Convert labels to their numpy representations
+	Y_train = Y_train.values
+	Y_validation = Y_validation.values
+
+	# Zero padding
+	for dataset, side in itertools.product([X_train, X_validation], ['left', 'right']):
+		dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
+
+	# Make sure everything is ok
+	assert X_train['left'].shape == X_train['right'].shape
+	assert len(X_train['left']) == len(Y_train)
+
+	# Model variables
+	n_hidden = 50
+	gradient_clipping_norm = 1.25
+	batch_size = 64
+	n_epoch = 4
+
+	# The visible layer
+	left_input = Input(shape=(max_seq_length,), dtype='int32')
+	right_input = Input(shape=(max_seq_length,), dtype='int32')
+
+	embedding_layer = Embedding(len(embeddings), embedding_dim, weights=[embeddings], input_length=max_seq_length, trainable=False)
+
+	# Embedded version of the inputs
+	encoded_left = embedding_layer(left_input)
+	encoded_right = embedding_layer(right_input)
+
+	# Since this is a siamese network, both sides share the same LSTM
+	shared_lstm = LSTM(n_hidden)
+
+	left_output = shared_lstm(encoded_left)
+	right_output = shared_lstm(encoded_right)
+
+
+	malstm_distance = Merge(mode=lambda x: K.exp(-K.sum(K.abs(x[0]-x[1]), axis=1, keepdims=True)), output_shape=lambda x: (x[0][0], 1))([left_output, right_output])
+
+	# Pack it all up into a model
+	malstm = Model([left_input, right_input], [malstm_distance])
+
+	malstm.save('lstm_man_model_char.h5')
+
+	# Adadelta optimizer, with gradient clipping by norm
+	optimizer = Adadelta(clipnorm=gradient_clipping_norm)
+
+	malstm.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
+	# Start training
+	training_start_time = time()
+
+	malstm_trained = malstm.fit([X_train['left'], X_train['right']], Y_train, batch_size=batch_size, epochs=n_epoch, verbose = 2,
+		                        validation_data=([X_validation['left'], X_validation['right']], Y_validation))
+
+	#print("Training time finished.\n{} epochs in {}".format(n_epoch, datetime.timedelta(seconds=time()-training_start_time)))
+
+
+	# Plot accuracy
+	plt.plot(malstm_trained.history['acc'])
+	plt.plot(malstm_trained.history['val_acc'])
+	plt.title('Model Accuracy')
+	plt.ylabel('Accuracy')
+	plt.xlabel('Epoch')
+	plt.legend(['Train', 'Validation'], loc='upper left')
+	plt.savefig('siamese_lstm_accuracy.png')
+	# Plot loss
+	plt.figure(2)
+	plt.plot(malstm_trained.history['loss'])
+	plt.plot(malstm_trained.history['val_loss'])
+	plt.title('Model Loss')
+	plt.ylabel('Loss')
+	plt.xlabel('Epoch')
+	plt.legend(['Train', 'Validation'], loc='upper right')
+	plt.savefig('siamese_lstm_loss.png')
+
+
+elif(sys.argv[1] == 'test'):
+	char_model = {}
+	# File paths
+	TEST_CSV = './test.csv'
+
+	# Load training and test set
+	test_df = pd.read_csv(TEST_CSV)
+
+	# loading pre-traing char2vec
+	if os.path.isfile('char_model.pickle'):
+		handle = open('char_model.pickle', 'rb')
+		char_model = pickle.load(handle,encoding='latin1')
+	else:
+		char_model = loadGloveModel('./glove.6B.300d-char.txt')
+
+	# Prepare embedding
+	vocabulary = dict()
+	inverse_vocabulary = ['<unk>']  # '<unk>' will never be used, it is only a placeholder for the [0, 0, ....0] embedding
+
+	questions_cols = ['question1', 'question2']
+
+	# Iterate over the questions only of both training and test datasets
+	for dataset in [test_df]:
+		for index, row in dataset.iterrows():
+
+		    # Iterate through the text of both questions of the row
+		    for question in questions_cols:
+
+		        q2n = []  # q2n -> question numbers representation
+		        for char in text_to_char_list(row[question]):
+
+		            # Check for unwanted chars
+		            if char not in char_model:
+		                continue
+
+		            if char not in vocabulary:
+		                vocabulary[char] = len(inverse_vocabulary)
+		                q2n.append(len(inverse_vocabulary))
+		                inverse_vocabulary.append(char)
+		            else:
+		                q2n.append(vocabulary[char])
+
+		        # Replace questions as char to question as number representation
+		        dataset.set_value(index, question, q2n)
+
+		
+	embedding_dim = 300
+	embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)  # This will be the embedding matrix
+	embeddings[0] = 0  # So that the padding will be ignored
+
+
+	# Build the embedding matrix
+	for char, index in vocabulary.items():
+		if char in char_model:
+		    embeddings[index] = char_model[char]
+
+	max_seq_length = 500
+
+	X_test = test_df[questions_cols]
+	Y_test = test_df['is_duplicate']
+
+
+	X_test = {'left': test_df.question1, 'right': test_df.question2}
+
+	# Convert labels to their numpy representations
+	Y_test = Y_test.values	
+
+	# Zero padding
+	for dataset, side in itertools.product([X_test], ['left', 'right']):
+		dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
+
+	# Make sure everything is ok
+	assert X_test['left'].shape == X_test['right'].shape
+	assert len(X_test['left']) == len(Y_test)
+
+	
+	gradient_clipping_norm = 1.25
+
+	malstm_model_trained = load_model('lstm_man_model_char.h5')
+	optimizer = Adadelta(clipnorm=gradient_clipping_norm)
+	malstm_model_trained.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
+	malstm_test = malstm_model_trained.evaluate([X_test['left'], X_test['right']], Y_test, batch_size=None, verbose=1, sample_weight=None, steps=None)
+	print (malstm_test)
+	
+
